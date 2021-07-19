838ccbebcd956ad95be662cff5fc7e1243a18306
abd96 <abdulhamed.chribati@tu-dortmund.de>
2021-07-20 00:50:44 +0200

    modify

--- lua/lspsaga-config.lua
+++ lua/lspsaga-config.lua
@@ -1,15 +1,25 @@
+--local saga = require 'lspsaga'
+--saga.init_lsp_saga()
+--
+--require'lspsaga.diagnostic'.show_line_diagnostics()
+--
+--vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+--  vim.lsp.diagnostic.on_publish_diagnostics, {
+--    virtual_text = false,
+--    underline = true,
+--    signs = true,
+--  }
+--)
+--vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
+--vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
+
+
 local saga = require 'lspsaga'
-saga.init_lsp_saga()
 
-require'lspsaga.diagnostic'.show_line_diagnostics()
-
-vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-    underline = true,
-    signs = true,
-  }
-)
-vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
-vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
-
+saga.init_lsp_saga {
+  error_sign = '',
+  warn_sign = '',
+  hint_sign = '',
+  infor_sign = '',
+  border_style = "round",
+}
