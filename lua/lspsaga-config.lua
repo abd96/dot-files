6bf16707b9519d4422c8df7d3d54a9ec8645d8a5
abd96 <abdulhamed.chribati@tu-dortmund.de>
2021-07-19 18:47:37 +0200

    added lua folder

---
+++ lua/lspsaga-config.lua
@@ -0,0 +1,15 @@
+local saga = require 'lspsaga'
+saga.init_lsp_saga()
+
+require'lspsaga.diagnostic'.show_line_diagnostics()
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, {
+    virtual_text = false,
+    underline = true,
+    signs = true,
+  }
+)
+vim.cmd [[autocmd CursorHold * lua vim.lsp.diagnostic.show_line_diagnostics()]]
+vim.cmd [[autocmd CursorHoldI * silent! lua vim.lsp.buf.signature_help()]]
+
